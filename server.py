63048e73c2536756c57887b11bbfc5c0f055609e
sillascoelho <sillasalmeidacoelho@gmail.com>
2024-05-08 15:04:57 -0300

    Gerando nova página

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def estacaZero():
 
-    return render_template('main.html')
+    return render_template('resultados.html')
 
 @app.route('/submitEstaca', methods=['POST', 'GET'])
 
